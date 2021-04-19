7d69651a0930b13b45ea53599a141c77cf984fb7
Laerm-Monitoring <81957838+Laerm-Monitoring@users.noreply.github.com>
2021-04-19 15:15:12 +0200

    Update laermmonitoring.cmd

--- laermmonitoring.cmd
+++ laermmonitoring.cmd
@@ -1,6 +1,6 @@
 @echo off
 chcp 1252 
-REM Windows-Codepage 1252 für Umlaute in Konsole
+REM Windows-Codepage 1252 fÃ¼r Umlaute in Konsole
 
 REM psql Ordner in Pfad aufnehmen
 set path=c:\Program Files\PostgreSQL\13\bin;%path%
@@ -12,7 +12,7 @@ SET PGPASSWORD=12345
 REM alternativ: SET PGUSER=postgres mit vergebenem Passwort bei Installation
 
 
-REM Gibtg es Datenbank laermmonitoring?
+REM Gibt es Datenbank laermmonitoring?
 psql -l -A | find "laermmonitoring|"
 if errorlevel 1 (
    Echo Erzeuge Datenbank laermmonitoring
@@ -26,7 +26,7 @@ REM Nur Auspacken
 REM powershell -Command "& {Expand-Archive -LiteralPath $env:USERPROFILE\Downloads\train.zip     -DestinationPath $env:USERPROFILE\Downloads -Force}"
 REM powershell -Command "& {Expand-Archive -LiteralPath $env:USERPROFILE\Downloads\train_all.zip -DestinationPath $env:USERPROFILE\Downloads -Force}"
 
-REM Alles löschen
+REM Alles lÃ¶schen
 echo on
 psql -c "DELETE FROM train" laermmonitoring user
 
@@ -34,7 +34,7 @@ psql -c "DELETE FROM train" laermmonitoring user
 @REM Importiere Daten von train.csv
 type %USERPROFILE%\Downloads\train.csv | psql -c "copy train (location_id,track, entry_time, directionok, category, duration_ds, len_dm, speed_km_h, lafmax10_cb, laeq10_cb, info) from stdin WITH CSV HEADER" laermmonitoring
 
-@REM Importiere Daten von train_all.csv, Spaltennamen müssen nicht angegeben werden, da in csv alle enthalten sind.
+@REM Importiere Daten von train_all.csv, Spaltennamen mÃ¼ssen nicht angegeben werden, da in csv alle enthalten sind.
 @REM type %USERPROFILE%\Downloads\train_all.csv | psql -c "copy train from stdin WITH CSV HEADER" laermmonitoring
 
 pause
